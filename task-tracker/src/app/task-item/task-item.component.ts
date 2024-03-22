14f6a69b455dc52b9d6960de58146b30d39ec690
Marigza <marigola@mail.ru>
2024-03-22 22:04:29 +0300

    feat: add base components

---
+++ task-tracker/src/app/task-item/task-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tta-task-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './task-item.component.html',
+  styleUrl: './task-item.component.scss'
+})
+export class TaskItemComponent {
+
+}
